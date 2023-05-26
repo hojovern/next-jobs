0b2f946e7824da8c9c4b0662af354685979d84ce
Mika <mika@Mikas-MacBook-Pro.local>
2023-05-26 15:18:55 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"), 
+  require('@tailwindcss/typography')],
+  
+ // daisyUI config (optional)
+ daisyui: {
+  // themes: ["cmyk", "dracula"],
+  darkTheme: "cupcake",
+},
+}
+
